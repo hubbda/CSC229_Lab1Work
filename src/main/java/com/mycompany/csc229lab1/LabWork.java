39762adb444c58543d247acee8657601c300ac1f
Devon Hubbard <devon.hubbd@gmail.com>
2022-02-02 16:48:53 -0500

    added all 3 tasks

--- src/main/java/com/mycompany/csc229lab1/LabWork.java
+++ src/main/java/com/mycompany/csc229lab1/LabWork.java
@@ -10,5 +10,42 @@ package com.mycompany.csc229lab1;
  * @author devon
  */
 public class LabWork {
+    static int getSumOfPrimes(int n) {
+        int sum = 0;
+        for(int i=2; i <=n; i++)
+            if(isPrim(i))
+                sum = sum+ i;
+        
+        return sum;
+    }
+    static boolean isPrim(int x) {
+        for(int i=2; i<x; i++)
+            if(x%i == 0)
+                return false;
+        return true;
+    }
+    static boolean checkDigit(int x, int digit) {
+        String str = ""+x;
+        return str.contains(""+digit);
+    }
     
+    static int getSumofPrimeWithDigit(int n) {
+        int sum=0;
+        for(int i=2; i<=n; i++)
+            if((isPrim(i) &&checkDigit(i, 3)))
+                sum = sum+1;
+        return sum;
+    }
+    static int getSumofPrimeWithTwoDigits(int n) {
+        int sum=0;
+        for(int i=2; i<=n; i++)
+            if(isPrim(i) &&checkDigit(i, 3) &&checkDigit(i, 5)) {
+                sum = sum+i;
+            }
+        return sum;
+    }
+
+    public static void main(String[] args ) {
+        System.out.println(getSumofPrimeWithTwoDigits(15) );
+    }
 }
